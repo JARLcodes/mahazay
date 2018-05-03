8fb6f11324727b3189a960ba2906b9ecb87e33f6
Abigail Demsas <ademsas1026@gmail.com>
2018-05-03 16:02:57 -0400

    single entry before create react app

--- src/components/SingleEntry.jsx
+++ src/components/SingleEntry.jsx
@@ -1,6 +1,12 @@
 import React from "react";
-import {Editor, EditorState, RichUtils} from "draft-js";
+import {EditorState, RichUtils, convertFromRaw} from "draft-js";
+import Editor, { composeDecorators } from 'draft-js-plugins-editor';
 import Button from 'material-ui/Button';
+import createImagePlugin from 'draft-js-image-plugin';
+import createFocusPlugin from 'draft-js-focus-plugin';
+import createBlockDndPlugin from 'draft-js-drag-n-drop-plugin'
+
+import SingleEntrySidebar from './SingleEntrySidebar.jsx';
 
 
 const styleMap = {
@@ -12,7 +18,66 @@ const styleMap = {
   }
 };
 
-export default class App extends React.Component {
+const focusPlugin = createFocusPlugin();
+const blockDndPlugin = createBlockDndPlugin();
+
+const decorator = composeDecorators(
+  focusPlugin.decorator,
+  blockDndPlugin.decorator
+);
+
+const imagePlugin = createImagePlugin({ decorator });
+
+const plugins = [
+  blockDndPlugin,
+  focusPlugin,
+  imagePlugin
+];
+
+/* eslint-disable */
+const initialState = {
+  "entityMap": {
+      "0": {
+          "type": "IMAGE",
+          "mutability": "IMMUTABLE",
+          "data": {
+              "src": "/images/canada-landscape-small.jpg"
+          }
+      }
+  },
+  "blocks": [{
+      "key": "9gm3s",
+      "text": "You can have images in your text field which are draggable. Hover over the image press down your mouse button and drag it to another position inside the editor.",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {}
+  }, {
+      "key": "ov7r",
+      "text": " ",
+      "type": "atomic",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [{
+          "offset": 0,
+          "length": 1,
+          "key": 0
+      }],
+      "data": {}
+  }, {
+      "key": "e23a8",
+      "text": "You can checkout the alignment tool plugin documentation to see how to build a compatible block plugin …",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {}
+  }]
+};
+/* eslint-enable */
+
+export default class SingleEntry extends React.Component {
   constructor(props) {
     super(props);
     this.state = {editorState: EditorState.createEmpty()};
@@ -30,17 +95,21 @@ export default class App extends React.Component {
     
     return (
       <div>
-        <Button onClick={this.onStyleClick.bind(this, 'BOLD')}>Bold</Button>
-        <Button onClick={this.onStyleClick.bind(this, 'ITALIC')}>Italic</Button>
-        <Button onClick={this.onStyleClick.bind(this, 'UNDERLINE')}>Underline</Button>
-        <Button onClick={this.onStyleClick.bind(this, 'STRIKETHROUGH')}>Strikethrough</Button>
-        <div>
-          <Editor
-            customStyleMap={styleMap}
-            editorState={this.state.editorState}
-            onChange={this.onChange}
-            placeholder="...start here"
-          />
+        <SingleEntrySidebar/>
+        <div id="editor">
+          <Button onClick={this.onStyleClick.bind(this, 'BOLD')}>Bold</Button>
+          <Button onClick={this.onStyleClick.bind(this, 'ITALIC')}>Italic</Button>
+          <Button onClick={this.onStyleClick.bind(this, 'UNDERLINE')}>Underline</Button>
+          <Button onClick={this.onStyleClick.bind(this, 'STRIKETHROUGH')}>Strikethrough</Button>
+          <div>
+            <Editor
+              customStyleMap={styleMap}
+              editorState={this.state.editorState}
+              onChange={this.onChange}
+              placeholder="...start here"
+              plugins={plugins}
+            />
+          </div>
         </div>
       </div>
     );
---
+++ src/components/SingleEntrySidebar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SingleEntrySidebar extends Component {
+  render() {
+    return (
+      <div id="sidebar">
+        
+      </div>
+    )
+  }
+}
